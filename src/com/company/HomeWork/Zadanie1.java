66ffe5113543c81766d91d6a7cc275111a4524d2
evgeni <gg1786@rambler.ru>
2019-10-20 23:23:09 +0200

    first Task

---
+++ src/com/company/HomeWork/Zadanie1.java
@@ -0,0 +1,31 @@
+package com.company.HomeWork;
+
+import java.util.Scanner;
+
+public class Zadanie1 {
+    public static void main(String[] args) {
+        System.out.println("Введите положительное число: ");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 2; i < N; i++) {
+            if (checkSimple(i))
+                System.out.println(i);
+        }
+    }
+
+    public static boolean checkSimple(int i) {
+        if (i <= 1)
+            return false;
+        else if (i <= 3)
+            return true;
+        else if (i % 2 == 0 || i % 3 == 0)
+            return false;
+        int n = 5;
+        while (n * n <= i) {
+            if (i % n == 0 || i % (n + 2) == 0)
+                return false;
+            n = n + 6;
+        }
+        return true;
+    }
+}
