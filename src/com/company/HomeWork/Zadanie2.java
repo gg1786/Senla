836fbeae6192e7ed1bc659197c1f3edbe7b0b5ae
evgeni <gg1786@rambler.ru>
2019-10-20 23:46:54 +0200

    Second Task

---
+++ src/com/company/HomeWork/Zadanie2.java
@@ -0,0 +1,26 @@
+package com.company.HomeWork;
+
+import java.util.Scanner;
+
+public class Zadanie2 {
+
+   public static int f(int n){
+       if(n==0){
+           return 0;
+       }else if(n==1){
+           return 1;
+       }else {
+           return f(n-1)+ f(n-2);
+       }
+   }
+
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int fn = f(n);
+
+        System.out.println("Искомое число: " + fn);
+    }
+}
