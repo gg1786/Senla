98d3d6970aa61577b5d476e41f567d8a062aed94
evgeni <gg1786@rambler.ru>
2019-10-21 23:37:24 +0200

    Fifth Task

---
+++ src/com/company/HomeWork/Zadanie5.java
@@ -0,0 +1,30 @@
+package com.company.HomeWork;
+
+import java.util.Scanner;
+
+public class Zadanie5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        isPalindrome(s);
+    }
+
+    public static String reverseString(String s) {
+        String r = "";
+        for (int i = s.length() - 1; i >= 0; --i)
+            r += s.charAt(i);
+        return r;
+
+
+    }
+
+    public static Boolean isPalindrome(String s) {
+        if (s.equals(reverseString(s))) {
+            System.out.println("Полиндром");
+        } else {
+            System.out.println("Не полиндром");
+        }
+        return s.equals(reverseString(s));
+
+    }
+}
